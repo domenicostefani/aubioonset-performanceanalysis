00338a171dbe2a0c6df0d737ea8dd392723c0aad
Domenico Stefani <domenico.stefani@studenti.unitn.it>
2020-11-10 11:45:01 +0100

    comment and simplfy

--- computeDelay.py
+++ computeDelay.py
@@ -1,15 +1,46 @@
 #!/usr/bin/env python3
 
-import glob
-import os
-import re
-from enum import Enum
+## Compute the AUBIOONSET delay with different parameters
+#
+# ** This is intended to be used with different support script
+#    and placed in its original repository directory.
+#
+# This script does multiple things:
+# - It asks the user for the parameters for AUBIOONSET
+# - It calls "extractAllOnsets.sh" with those parameters, which calls aubioonset
+#   on all the WAV files in the folder
+# - It compares the onsets labeled with the ones extracted
+#   When computing the delay, it sums the delay value that AUBIOONSET regularly
+#   subtracts to the detection time, to center the distribution around 0
+# - It finally calls a R analysis script to plot the delay distribution and the
+#   metrics (Accuracy, Precision, Recall)
+#
+# Author: Domenico Stefani
+#         domenico.stefani[at]unitn.it
+#         domenico.stefani96[at]gmail.com
+# Date:   10th Nov 2020
+#
+##
 
-class ParamType(Enum):
+import glob             # To read folder filelist
+import os               # To call scripts
+import re               # Regexp, to parse script results
+from enum import Enum   # To specify parameter type
+
+# *Main comparation hyperparameter*
+# This states the maximum time delay between labeled and detected onset
+# in order for them to be considered the same onset
+# Onsets further apart than this interval will be considered different
+MAX_ONSET_DIFFERENCE_S = 0.02
+print("Onset further apart than " + str(MAX_ONSET_DIFFERENCE_S*1000.0) + \
+      "ms are considered different")
+
+class ParamType(Enum):  # Aubioonset parameter type
     INT = 1
     FLOAT = 2
     STR = 3
 
+# Ask the user to input the parameter, or use default if none is specified
 def readParam(param_name,default_value,param_type):
     try:
         param_str = input(param_name+"(default: " + str(default_value) + "): ")
@@ -31,9 +62,9 @@ def readParam(param_name,default_value,param_type):
         print("ValueError")
 
 
+# Read all the parameters
 print("Extracting all the onsets from the wav files in this folder")
 print("Specify the parameter values or press <ENTER> for default")
-
 BUFFER_SIZE = readParam("BUFFER_SIZE",256,ParamType.INT)
 HOP_SIZE = readParam("HOP_SIZE",128,ParamType.INT)
 SILENCE_THRESHOLD = readParam("SILENCE_THRESHOLD",-40.0,ParamType.FLOAT)
@@ -42,35 +73,44 @@ print("Available methods:<default|energy|hfc|complex|phase|specdiff|kl|mkl|specf
 ONSET_METHOD = readParam("ONSET_METHOD","default",ParamType.STR)
 MINIMUM_INTER_ONSET_INTERVAL_SECONDS = readParam("MINIMUM_INTER_ONSET_INTERVAL_SECONDS",0.020,ParamType.FLOAT)
 
-opts = "-B " + str(BUFFER_SIZE) + " -H " + str(HOP_SIZE) + " -s " + str(SILENCE_THRESHOLD) + " -t " + str(ONSET_THRESHOLD) + " -O " + str(ONSET_METHOD) + " -M " + str(MINIMUM_INTER_ONSET_INTERVAL_SECONDS) + ""
+# Create the option string with the parameter values specified
+opts = " -B " + str(BUFFER_SIZE) + \
+       " -H " + str(HOP_SIZE) + \
+       " -s " + str(SILENCE_THRESHOLD) + \
+       " -t " + str(ONSET_THRESHOLD) + \
+       " -O " + str(ONSET_METHOD) + \
+       " -M " + str(MINIMUM_INTER_ONSET_INTERVAL_SECONDS) + ""
 
+# Call onset extraction routine
 EXT_RES = os.popen("./utility_scripts/extractAllOnsets.sh " + opts).read()
 
+# Parse script output, looking for the AUBIOONSET delay (in samples)
 AUBIODELAY_SAMPLES = int(re.search('[0-9]+', EXT_RES).group(0))
 print("The delay introduced by aubioonset is " + str(AUBIODELAY_SAMPLES) + " samples")
 SAMPLE_RATE = 48000
 AUBIODELAY_S = AUBIODELAY_SAMPLES * 1.0 / SAMPLE_RATE
 print("Delay in ms " + str(AUBIODELAY_S*1000.0))
 
-MAX_ONSET_DIFFERENCE_S = 0.3 # Onsets further apart than this interval will be considered different
+# Strings for CSV separator and Not-a-Number values
+SEP_STR = ","   # separator
+NAN_STR = "NAN" # NaN
 
-print("Onset further apart than " + str(MAX_ONSET_DIFFERENCE_S*1000.0) + "ms are considered different")
-
-NAN_STR = "NAN"
-SEP_STR = ","
-
+# Compare the onsets in @labels_file and @extracted_file, compute the delay and
+# write the result as a CSV to out_file
 def computeDifference(labels_file,extracted_file,out_file):
-    end_flag = False
-    lbl_line = labels_file.readline()
-    ext_line = extracted_file.readline()
+    end_flag = False                     # termination flag
+    lbl_line = labels_file.readline()    # read the very first line
+    ext_line = extracted_file.readline() # read the very first line
     while not end_flag:
-        if lbl_line is "" and ext_line is "": #TODO check closing condition
+        if lbl_line is "" and ext_line is "": # Terminate when both are EOF
             end_flag = True
         else:
+            # Convert to float or set to infinity if EOF
             lbl_value = float(lbl_line.split()[0] if (lbl_line is not "") else "inf")
             ext_value = float(ext_line.split()[0] if (ext_line is not "") else "inf")
+            # Compute the delay in seconds and sum AUBIOONSET delay
             diff = ext_value - lbl_value + AUBIODELAY_S
-
+            # Skip values if onsets are different (delay greater than threshold)
             if abs(diff) < MAX_ONSET_DIFFERENCE_S:
                 out_file.write(str(lbl_value) + SEP_STR + str(ext_value) + SEP_STR + str(diff) + "\n")
                 lbl_line = labels_file.readline()
@@ -82,14 +122,16 @@ def computeDifference(labels_file,extracted_file,out_file):
                 out_file.write(NAN_STR + SEP_STR + str(ext_value) + SEP_STR + NAN_STR + "\n")
                 ext_line = extracted_file.readline()
 
+# Print info
 print("Computing the delays for:")
-
 onsets_labeled = glob.glob("onsets_labeled/*.txt")
+# Open output file
 OUT_FILENAME="output/onset_delay.csv"
 output_csv = open(OUT_FILENAME, "w")
+# Write CSV header
 output_csv.write("onset_labeled" + SEP_STR + "onset_extracted" + SEP_STR + "difference\n")
 
-
+# Iterate over all label files and call computeDifference() for all files
 for filename in onsets_labeled:
     print(filename)
     filename = os.path.basename(filename)
@@ -103,5 +145,6 @@ output_csv.close()
 
 print("Output can be found at " + OUT_FILENAME)
 
+# Call analysis script
 print("Calling R analysis script")
 os.system("Rscript utility_scripts/r_analysis/analize_delays.r")
