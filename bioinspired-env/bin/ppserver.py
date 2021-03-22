c2bc40fe5030b91102f6c00b5c07dce8eb57aaab
domenicostefani <domenico.stefani@unitn.it>
2021-03-22 19:17:46 +0100

    remove bioinspired venv

--- bioinspired-env/bin/ppserver.py
+++
@@ -1,4 +0,0 @@
-#!/home/cimil-01/Develop/aubioonset-performanceanalysis/bioinspired-env/bin/python3
-# EASY-INSTALL-SCRIPT: 'pp==1.6.4.4','ppserver.py'
-__requires__ = 'pp==1.6.4.4'
-__import__('pkg_resources').run_script('pp==1.6.4.4', 'ppserver.py')
